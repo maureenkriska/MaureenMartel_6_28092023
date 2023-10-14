351b822d50b218c4a555ead7da756a33b8a2d822
Maureen Kriska Martel <martel.maureen@orange.fr>
2023-10-14 18:31:38 +0200

    Création de mon modèle d'utilisateurs et installation de mongoose unique validator + bcrypt

---
+++ backend/models/user.js
@@ -0,0 +1,16 @@
+// Appel de mongoose
+const mongoose = require('mongoose')
+
+// Installation de Mongoose Unique Validator pour limiter les comptes avec la même adresse mail
+const uniqueValidator = require('mongoose-unique-validator')
+
+// Création de mon schéma de données utilisateurs
+const userSchema = mongoose.Schema({
+  email : { type: String, required: true, unique: true },
+  password : { type: String, required: true }
+})
+
+userSchema.plugin(uniqueValidator)
+
+// Export de mon modèle
+module.exports = mongoose.model('User', userSchema)
