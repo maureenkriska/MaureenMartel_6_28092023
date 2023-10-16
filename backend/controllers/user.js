41628328992876ac9f81a8f87f851ea6f469b6a1
Maureen Kriska Martel <martel.maureen@orange.fr>
2023-10-16 18:25:18 +0200

    Implémentation de mes fonctions signup et login

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
+
 exports.signup = (req, res, next) => {
   bcrypt.hash(req.body.password, 10) //Fonction hachage de bcrypt, salage du mdp 10 fois
     .then(hash => {
@@ -9,7 +12,31 @@ exports.signup = (req, res, next) => {
         .then(() => res.status(201).json({message: 'Utilsateur créé!'})) // Création du user et enregistrement dans ma BDD
         .catch(error => res.status(400).json({ error }))
     })
-    .catch(error => res.status(500).json({ error }))
+    .catch((error) => res.status(500).json({ error }))
 }
 
-//exports.login = (req, res, next) => {}
+exports.login = (req, res, next) => {
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if(!user) {
+        // J'utilise mon modèle pour vérifier si l'email entré par l'utilisateur correspond a un user existant de ma BDD
+        return res.status(401).json({ message: 'Identifiant ou mot de passe incorrect'}) 
+        // Si ce n'est pas le cas, j'envoie une erreur 401 Unauthorized, sinon je continue :
+      }
+      // J'utilise la fonction compare pour comparer le mdp entré par l'utilisateur avec le hash enregistré dans ma BDD
+      bcrypt.compare(req.body.password, user.password)
+        .then((valid) => {
+          if(!valid) {
+            // S'ils ne correspondent pas => Erreur 401 Unauthorized
+            return res.status(401).json({ message: 'Identifiant ou mot de passe incorrect'})
+          }
+          // S'ils correspondent j'envoie une réponse 200 contenant l'ID user et un token
+          res.status(200).json({
+            userId: user._id,
+            token: 'TOKEN'
+          })
+        })
+        .catch((error) => res.status(500).json({ error }))
+    })
+    .catch((error) => res.status(500).json({ error }))
+}
