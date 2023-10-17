e89632ca2ec40536c2742de95c727f6a064632fc
Maureen Kriska Martel <martel.maureen@orange.fr>
2023-10-17 18:13:22 +0200

    Ajout des token d'authentification + jsonwebtoken

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 exports.signup = (req, res, next) => {
   bcrypt.hash(req.body.password, 10) //Fonction hachage de bcrypt, salage du mdp 10 fois
@@ -27,13 +28,17 @@ exports.login = (req, res, next) => {
       bcrypt.compare(req.body.password, user.password)
         .then((valid) => {
           if(!valid) {
-            // S'ils ne correspondent pas => Erreur 401 Unauthorized
+          // S'ils ne correspondent pas => Erreur 401 Unauthorized
             return res.status(401).json({ message: 'Identifiant ou mot de passe incorrect'})
           }
           // S'ils correspondent j'envoie une réponse 200 contenant l'ID user et un token
           res.status(200).json({
             userId: user._id,
-            token: 'TOKEN'
+            token: jwt.sign(            // J'utilise la fonction sign de jsonwebtoken pour chiffrer un nouveau token
+                { userId: user._id },   // Mon token contient l'ID de mon utilisateur
+                SECRET_KEY,             // Je crypte mon token
+                { expiresIn: '24h' }    // Mon utilisateur devra se reconnecter au bout de 24h
+            )
           })
         })
         .catch((error) => res.status(500).json({ error }))
