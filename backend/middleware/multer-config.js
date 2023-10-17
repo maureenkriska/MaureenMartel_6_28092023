f4d26399249f0638f08dfe10b65442b183f33196
Maureen Kriska Martel <martel.maureen@orange.fr>
2023-10-17 19:13:03 +0200

    Configuration de multer

---
+++ backend/middleware/multer-config.js
@@ -0,0 +1,22 @@
+const multer = require('multer')
+
+const MIME_TYPES = {
+  'image/jpeg': 'jpg',
+  'image/jpg': 'jpg',
+  'image/png': 'png',  
+}
+
+const storage = multer.diskStorage({        // Contient la logique pour indiquer à multer où enregistrer les fichiers entrants
+  destination: (req,file,callback) => {     // Destination indique à multer d'enregistrer les fichiers dans le dossier images
+    callback(null, 'images')
+  },
+  filename: (req,file,callback) => {                      // Filename indique à multer d'utiliser le nom d'origine,
+    const name = file.originalname.split(' ').join('_')   // de remplacer les espaces par des underscores,
+    const extension = MIME_TYPES[file.mimetype]           // d'insérer l'extension de fichier appropriée,
+    callback(null, name + Date.now() + '.' + extension)   // et d'ajouter un timestamp comme nom de fichier.
+  }
+})
+
+
+// Exportation de multer configuré et indication de générer uniquement les téléchargements de fichiers image
+module.exports = multer({storage: storage}).single('images')
