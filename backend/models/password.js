ea3029fe2e47e2dd9836a0ed450ba1bc635699df
Maureen Kriska Martel <martel.maureen@orange.fr>
2023-11-02 15:16:12 +0100

    Modeles des sauces

--- backend/models/password.js
+++ backend/models/password.js
@@ -5,9 +5,9 @@ const pwdSchema = new pwdValidator()
 pwdSchema
 .is().min(4)            // Minimum 4 caractères
 .is().max(16)           // Maximum 16 caractères
-.has().uppercase()      // Contient une ou plusieurs majuscules
-.has().lowercase()      // Contient une ou plusieurs minuscules
-.has().digits()         // Contient un ou plusieur chiffres
+.has().uppercase()      // Contient au moins une majuscule
+.has().lowercase()      // Contient au moins une minuscule
+.has().digits()         // Contient au moins un chiffre
 .has().not().spaces()   // Ne dois pas contenir d'espaces
 
 module.exports = pwdSchema
